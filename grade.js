e4ef924f739c5294228c4cb84b1e752ac82b588a
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-24 20:41:53 +0300

    completed assignment

---
+++ grade.js
@@ -0,0 +1,19 @@
+// Generates students grade once marks are input.
+
+const input = prompt("Enter the student's marks (0-100):");
+
+function generateGrade() {
+    if (marks >89 && marks <=100);
+    grade = ("A");
+} else if(marks >70 && marks <=89);
+     grade = ("B")
+} else if(marks >60 && marks <=79);
+     grade = ("C");
+} else if(marks >50 && marks <=69);
+     grade = ("D");
+} else if(marks >40 && marks <=59);
+    grade = ("E");
+} else {
+    grade "invalid grade";
+}
+}
